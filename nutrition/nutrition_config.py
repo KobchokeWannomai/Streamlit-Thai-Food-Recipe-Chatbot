ad74008f3be91d015fd7cebe8acac8e0278a53af
Kobchoke <137274581+KobchokeWannomai@users.noreply.github.com>
2025-05-27 10:58:56 +0700

    Create nutrition_config.py

---
+++ nutrition/nutrition_config.py
@@ -0,0 +1,15 @@
+# config/nutrition_config.py
+NUTRITION_API_KEYS = {
+    'usda': 'udyaSCqY4F6wN4yzLJL4peSTHlGSxoDq5FoXHJTw',
+    'nutritionix': 'YOUR_NUTRITIONIX_API_KEY'
+}
+
+# การแมปชื่อวัตถุดิบไทย-อังกฤษ
+INGREDIENT_MAPPING = {
+    'กุ้งนาง': 'shrimp',
+    'หมู': 'pork',
+    'ไก่': 'chicken',
+    'ปลา': 'fish',
+    'ผักบุ้ง': 'water spinach',
+    # เพิ่มเติม...
+}
